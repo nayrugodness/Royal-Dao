9570deb667606e67884a92a6492c5a9145e0cf04
Nayru <nanualexandraramirez@gmail.com>
2022-07-15 12:15:15 -0500

    adding the drop membership address

--- scripts/4-set-claim-condition.js
+++ scripts/4-set-claim-condition.js
@@ -1,7 +1,7 @@
 import sdk from "./1-initialize-sdk.js";
 import { MaxUint256 } from "@ethersproject/constants";
 
-const editionDrop = sdk.getEditionDrop("INSERT_EDITION_DROP_ADDRESS");
+const editionDrop = sdk.getEditionDrop("0x6BF3E193B7297a3eB7E79dDc2e693875c81A1895");
 
 (async () => {
   try {
