ae050753dce55073e3b2c2fe956df415734d5ece
Nayru <nanualexandraramirez@gmail.com>
2022-07-15 17:30:54 -0500

    first app version deploy

--- src/App.jsx
+++ src/App.jsx
@@ -177,7 +177,7 @@ const App = () => {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>🌟 Welcome to Royal DAO 🌟</h1>
         <button onClick={connectWithMetamask} className="btn-hero">
           Connect your wallet
         </button>
