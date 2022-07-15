2def1ca3038926b014eb84f61a8ec55d2dbde75b
Nayru <nanualexandraramirez@gmail.com>
2022-07-15 12:32:15 -0500

    creating mint nft btn

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,11 @@ const App = () => {
   const editionDrop = useEditionDrop("0x6BF3E193B7297a3eB7E79dDc2e693875c81A1895");
   // State variable for us to know if user has our NFT.
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+  // isClaiming lets us easily keep a loading state while the NFT is minting.
+  const [isClaiming, setIsClaiming] = useState(false);
 
   useEffect(() => {
-    // If they don't have a connected wallet, exit!
+    // If they don't have an connected wallet, exit!
     if (!address) {
       return;
     }
@@ -36,7 +38,20 @@ const App = () => {
     checkBalance();
   }, [address, editionDrop]);
 
-  // ... include all your other code that was already there below.
+  const mintNft = async () => {
+    try {
+      setIsClaiming(true);
+      await editionDrop.claim("0", 1);
+      console.log(`🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0`);
+      setHasClaimedNFT(true);
+    } catch (error) {
+      setHasClaimedNFT(false);
+      console.error("Failed to mint NFT", error);
+    } finally {
+      setIsClaiming(false);
+    }
+  };
+
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
   if (!address) {
@@ -50,12 +65,18 @@ const App = () => {
     );
   }
 
-  // This is the case where we have the user's address
-  // which means they've connected their wallet to our site!
+  // Render mint nft screen.
   return (
-    <div className="landing">
-      <h1>👀 wallet connected, now what!</h1>
-    </div>);
+    <div className="mint-nft">
+      <h1>Mint your free 🍪DAO Membership NFT</h1>
+      <button
+        disabled={isClaiming}
+        onClick={mintNft}
+      >
+        {isClaiming ? "Minting..." : "Mint your nft (FREE)"}
+      </button>
+    </div>
+  );
 }
 
 export default App;
